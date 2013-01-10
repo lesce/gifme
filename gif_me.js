a8f71b3d6c4be124c4b83ed6d93ccd44022ab6e6
Lescenco Andrei Bogdan <andrei.lescenco@sparktech.ro>
2013-01-10 17:34:55 +0200

    added jquery

--- gif_me.js
+++ gif_me.js
@@ -1,14 +1,18 @@
-var req = new XMLHttpRequest();
-req.open(
-    "GET",
-    "https://api.imgur.com/3/album/0KukT/images.json",
-    true);
-req.setRequestHeader("Authorization","Client-ID 277282d5a28094d")
-req.setRequestHeader("Content-Type","application/json")
-req.send(null);
-showPhotos(req);
+$.ajax({
+    url: "https://api.imgur.com/3/album/0KukT/images.json",
+    type: "GET",
+    beforeSend: setHeader,
+    data: "",
+    success: showPhotos,
+    dataType: "json"
+});
 
-function showPhotos(req) {
-  console.log(req.response);
-  console.log(req);
+function setHeader(req){
+  req.setRequestHeader("Authorization","Client-ID 277282d5a28094d")
+}
+
+function showPhotos(data,status,x) {
+  console.log(data);
+  console.log(status);
+  console.log(x);
 }
