0e118befa5d9425312f32cc9c136da3ee29d9fce
Lescenco Andrei Bogdan <andrei.lescenco@sparktech.ro>
2013-01-10 17:14:26 +0200

    small changes for imgur instead of flickr

--- gif_me.js
+++ gif_me.js
@@ -1,33 +1,14 @@
 var req = new XMLHttpRequest();
 req.open(
     "GET",
-    "http://api.flickr.com/services/rest/?" +
-    "method=flickr.photos.search&" +
-    "api_key=90485e931f687a9b9c2a66bf58a3861a&" +
-    "text=hello%20world&" +
-    "safe_search=1&" +  // 1 is "safe"
-    "content_type=1&" +  // 1 is "photos only"
-    "sort=relevance&" +  // another good one is "interestingness-desc"
-    "per_page=20",
+    "https://api.imgur.com/3/album/0KukT/images.json",
     true);
-req.onload = showPhotos;
+req.setRequestHeader("Authorization","Client-ID 277282d5a28094d")
+req.setRequestHeader("Content-Type","application/json")
 req.send(null);
+showPhotos(req);
 
-function showPhotos() {
-  var photos = req.responseXML.getElementsByTagName("photo");
-
-  for (var i = 0, photo; photo = photos[i]; i++) {
-    var img = document.createElement("image");
-    img.src = constructImageURL(photo);
-    document.body.appendChild(img);
-  }
-}
-
-// See: http://www.flickr.com/services/api/misc.urls.html
-function constructImageURL(photo) {
-  return "http://farm" + photo.getAttribute("farm") +
-    ".static.flickr.com/" + photo.getAttribute("server") +
-    "/" + photo.getAttribute("id") +
-    "_" + photo.getAttribute("secret") +
-    "_s.jpg";
+function showPhotos(req) {
+  console.log(req.response);
+  console.log(req);
 }
